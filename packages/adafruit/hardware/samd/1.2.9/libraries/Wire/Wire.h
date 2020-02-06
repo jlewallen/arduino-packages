e6b0a1501b3a71e8b8556c31415bddfbf8cf90f1
Jacob Lewallen <jlewallen@gmail.com>
2020-02-05 16:56:38 -0800

    Quick hack for I2C buffer sizes...

--- packages/adafruit/hardware/samd/1.2.9/libraries/Wire/Wire.h
+++ packages/adafruit/hardware/samd/1.2.9/libraries/Wire/Wire.h
@@ -71,10 +71,10 @@ class TwoWire : public Stream
     bool transmissionBegun;
 
     // RX Buffer
-    RingBufferN<256> rxBuffer;
+    RingBufferN<2048> rxBuffer;
 
     //TX buffer
-    RingBufferN<256> txBuffer;
+    RingBufferN<2048> txBuffer;
     uint8_t txAddress;
 
     // Callback user functions
